8bcc625aa53033f8b8fbfab81fc36c6610f02581
Unicorn <mariazhingova@gmail.com>
2016-07-25 16:21:09 +0300

    cass ajusment on fanzone fage

---
+++ app/states/fan_zone/fanZone.js
@@ -0,0 +1,28 @@
+(function (angular) {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('fanZone', directive)
+        .config(config);
+
+    config.$inject = ['$stateProvider'];
+    function config($stateProvider) {
+        $stateProvider
+            .state('fanZone', {
+                url: '/',
+                title: 'Fan Zone',
+                template: '<fan-zone></fan-zone>'
+            });
+    }
+
+    function directive() {
+        var directive = {
+            templateUrl: './states/fan_zone/fanZone.html',
+            restrict: 'E',
+            scope: {}
+        };
+
+        return directive;
+    }
+}(angular));
